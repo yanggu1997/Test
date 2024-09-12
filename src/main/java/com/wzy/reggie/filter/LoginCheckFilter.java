a10241588ec86e733d8d2d26e8c74de56301983e
yanggu1997_wzy <yanggu1997@qq.com>
2024-09-12 21:56:39 +0800

    修改redis 为固定ip而非localhost

--- src/main/java/com/wzy/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/wzy/reggie/filter/LoginCheckFilter.java
@@ -36,6 +36,10 @@ public class LoginCheckFilter implements Filter {
                 "/front/**",
                 "/user/sendMsg",
                 "/user/login",
+                "/doc.html",
+                "/webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
         };
         //2、判断本次请求是否需要处理
         boolean check = check(Urls, requestURI);
